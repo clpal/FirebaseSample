44c06c2a85c75d7b8505744c87039fbcaf84120b
CL Pal <clpalmca@gmail.com>
2021-12-12 13:55:45 +0530

    crash Analytics report

--- app/src/main/java/com/clpal/khatabook/crashlytics/CrashlyticsActivity.kt
+++ app/src/main/java/com/clpal/khatabook/crashlytics/CrashlyticsActivity.kt
@@ -7,13 +7,10 @@ import com.clpal.khatabook.databinding.ActivityCrashlyticsBinding
 import java.lang.RuntimeException
 
 class CrashlyticsActivity : AppCompatActivity() {
-
-
     private lateinit var binding: ActivityCrashlyticsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityCrashlyticsBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
         binding.crashbutton.setOnClickListener {
             throw  RuntimeException("Application has been crash")
